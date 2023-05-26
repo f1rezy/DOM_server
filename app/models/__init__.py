d57f901f436c9012413ff87e275c03447fb2be8d
Daniil Litvinenko <danil.fire@bk.ru>
2023-05-26 22:14:27 +0700

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,32 @@
-
+from .city import City
+from .event import Event
+from .event_status import EventStatus
+from .field import Field
+from .file import File
+from .level import Level
+from .many_to_many_relations import *
+from .organization import Organization
+from .organization_role import OrganizationRole
+from .organization_role_user import OrganizationRoleUser
+from .region import Region
+from .role import Role
+from .subscription import Subscription
+from .user_info import UserInfo
 
 __all__ = [
+    "City",
+    "Event",
+    "EventStatus",
+    "Field",
+    "File",
+    "Level",
+    "event_to_field",
+    "event_to_user_info",
+    "Organization",
+    "OrganizationRole",
+    "OrganizationRoleUser",
+    "Region",
+    "Role",
+    "Subscription",
+    "UserInfo"
 ]
